190c34d6a93401c76c2cf344a32a0f4bfcc34c88
Rico <d-Rickyy-b@users.noreply.github.com>
2020-07-01 01:35:17 +0200

    feat(analyzer): implement PreCertAnalyzer
    This analyzer can be used to find PreCerts

---
+++ certpwn/analyzers/precertanalyzer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from .basicanalyzer import BasicAnalyzer
+
+
+class PreCertAnalyzer(BasicAnalyzer):
+    """
+    Analyzer for finding pre certificate updates
+    """
+    name = "PreCertAnalyzer"
+
+    def __init__(self, actions=None):
+        super().__init__(actions)
+
+    def match(self, update):
+        return update.update_type == "PrecertLogEntry"
