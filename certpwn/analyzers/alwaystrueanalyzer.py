b54dbbe23ec2f26de401ea87d5cad4d66a48e760
Rico <d-Rickyy-b@users.noreply.github.com>
2020-07-01 00:36:09 +0200

    feat(analyzer): implement AlwaysTrueAnalyzer

---
+++ certpwn/analyzers/alwaystrueanalyzer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from .basicanalyzer import BasicAnalyzer
+
+
+class AlwaysTrueAnalyzer(BasicAnalyzer):
+    """Analyzer which always matches an certificate update to perform actions on every certificate update"""
+    name = "AlwaysTrueAnalyzer"
+
+    def __init__(self, actions):
+        """
+        Analyzer which always matches a certificate update to perform actions on every certificate update
+        :param actions: A single action or a list of actions to be executed on every certificate update
+        """
+        super().__init__(actions)
+
+    def match(self, update):
+        """Always returns True to match every update available"""
+        return True
