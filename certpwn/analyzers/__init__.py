7573b1f89d5847d4ff890915ed9d0c8f1728dd42
Rico <d-Rickyy-b@users.noreply.github.com>
2020-07-01 01:41:25 +0200

    feat(analyzer): add imports to analyzer module

--- certpwn/analyzers/__init__.py
+++ certpwn/analyzers/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from .alwaystrueanalyzer import AlwaysTrueAnalyzer
 from .basicanalyzer import BasicAnalyzer
+from .cafingerprintanalyzer import CAFingerprintAnalyzer
+from .letsencryptanalyzer import LetsEncryptAnalyzer
+from .precertanalyzer import PreCertAnalyzer
 from .regexdomainanalyzer import RegexDomainAnalyzer
 from .x509analyzer import X509Analyzer
-__all__ = ['BasicAnalyzer', 'AlwaysTrueAnalyzer', 'X509Analyzer', 'RegexDomainAnalyzer']
 
+__all__ = ['BasicAnalyzer', 'AlwaysTrueAnalyzer', 'X509Analyzer', 'PreCertAnalyzer', 'CAFingerprintAnalyzer', 'LetsEncryptAnalyzer', 'RegexDomainAnalyzer']
