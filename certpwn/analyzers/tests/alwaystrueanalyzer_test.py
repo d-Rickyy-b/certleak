47fd66f26ad5bea0035510073e335d4a7a847cb8
Rico <d-Rickyy-b@users.noreply.github.com>
2020-07-01 17:09:58 +0200

    test: implement alwaystrueanalyzer test

---
+++ certpwn/analyzers/tests/alwaystrueanalyzer_test.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import unittest
+from unittest import mock
+
+from certpwn.actions.basicaction import BasicAction
+from certpwn.analyzers import AlwaysTrueAnalyzer
+
+
+class TestAlwaysTrueAnalyzer(unittest.TestCase):
+    def setUp(self):
+        self.analyzer = AlwaysTrueAnalyzer(None)
+        self.update = mock.Mock()
+
+    def test_match(self):
+        self.update.body = "Test"
+        self.assertTrue(self.analyzer.match(self.update))
+
+        self.update.body = None
+        self.assertTrue(self.analyzer.match(self.update))
+
+        self.update.body = ""
+        self.assertTrue(self.analyzer.match(self.update))
+
+        self.update = None
+        self.assertTrue(self.analyzer.match(self.update))
+
+    def test_actions_present(self):
+        action = mock.MagicMock(spec=BasicAction)
+        analyzer = AlwaysTrueAnalyzer(action)
+        self.assertEqual([action], analyzer.actions)
+
+
+if __name__ == '__main__':
+    unittest.main()
