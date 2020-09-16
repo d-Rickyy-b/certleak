5b8aeb5c1d5b69440b1451eed75ec95aa7740b6b
Rico <d-Rickyy-b@users.noreply.github.com>
2020-09-16 02:48:16 +0200

    feat: implement wrapper around request
    optimized for using  proxies

--- certpwn/util/__init__.py
+++ certpwn/util/__init__.py
@@ -4,5 +4,6 @@ from .threadingutils import start_thread, join_threads
 from .listify import listify
 from .dictwrapper import DictWrapper
 from .templatingengine import TemplatingEngine
+from .request import Request
 
-__all__ = ('listify', 'start_thread', 'join_threads', 'DictWrapper', 'TemplatingEngine')
+__all__ = ('listify', 'start_thread', 'join_threads', 'DictWrapper', 'TemplatingEngine', 'Request')
