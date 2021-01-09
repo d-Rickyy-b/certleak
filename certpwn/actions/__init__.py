fa1821416b17a40aadb1a322c0c91b43a2cac8a1
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-09 03:58:29 +0100

    feat: implement databaseaction

--- certpwn/actions/__init__.py
+++ certpwn/actions/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .basicaction import BasicAction
+from .databaseaction import DatabaseAction
 from .logaction import LogAction
 
-__all__ = ['BasicAction', 'LogAction']
+__all__ = ["BasicAction", "LogAction", "DatabaseAction"]
---
+++ certpwn/actions/databaseaction.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from .basicaction import BasicAction
+
+
+class DatabaseAction(BasicAction):
+    """Action to save a cert update to a database"""
+    name = "DatabaseAction"
+
+    def __init__(self, database):
+        super().__init__()
+        self.database = database
+
+    def perform(self, update, analyzer_name=None, matches=None):
+        """Store an incoming cert update in the database"""
+        self.database.store(update)
