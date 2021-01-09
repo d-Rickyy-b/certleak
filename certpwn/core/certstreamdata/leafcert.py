ae2aa0ce0a334e93a1bd24c531361a9b85098859
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-09 04:36:40 +0100

    refactor: adjust attributes in leafcert class

--- certpwn/core/certstreamdata/leafcert.py
+++ certpwn/core/certstreamdata/leafcert.py
@@ -7,7 +7,7 @@ from .subject import Subject
 class LeafCert(CertstreamObject):
     """Data class for the LeafCert data structure of certstream"""
 
-    def __init__(self, subject, extensions, not_before, not_after, serial_number, fingerprint, as_der, all_domains):
+    def __init__(self, subject, issuer, extensions, not_before, not_after, serial_number, fingerprint, signature_algorithm, all_domains):
         """
         Data class for the LeafCert data structure of certstream
         :param subject: Certificate subject
@@ -16,17 +16,18 @@ class LeafCert(CertstreamObject):
         :param not_after: 'Not after' validity field
         :param serial_number: Serial number of the certificate
         :param fingerprint: Certificate fingerprint
-        :param as_der: DER representation of the certificate
+        :param signature_algorithm: Signature algorithm
         :param all_domains: List of all domains contained in this cert
         """
         super().__init__()
         self.subject = subject
+        self.issuer = issuer
         self.extensions = extensions
         self.not_before = not_before
         self.not_after = not_after
         self.serial_number = serial_number
         self.fingerprint = fingerprint
-        self.as_der = as_der
+        self.signature_algorithm = signature_algorithm
         self.all_domains = all_domains
 
     @classmethod
@@ -41,23 +42,24 @@ class LeafCert(CertstreamObject):
 
         data = super(LeafCert, cls).from_dict(data)
         subject = Subject.from_dict(data.get("subject"))
+        issuer = Subject.from_dict(data.get("issuer"))
         extensions = Extensions.from_dict(data.get("extensions"))
         not_before = data.get("not_before")
         not_after = data.get("not_after")
         serial_number = data.get("serial_number")
         fingerprint = data.get("fingerprint")
-        as_der = data.get("as_der")
         all_domains = data.get("all_domains")
+        signature_algorithm = data.get("signature_algorithm")
 
         return cls(subject=subject,
+                   issuer=issuer,
                    extensions=extensions,
                    not_before=not_before,
                    not_after=not_after,
                    serial_number=serial_number,
                    fingerprint=fingerprint,
-                   as_der=as_der,
+                   signature_algorithm=signature_algorithm,
                    all_domains=all_domains)
 
     def __repr__(self):
-        return f"(subject: {self.subject}, extensions: {self.extensions}, not_before: {self.not_before}, not_after: {self.not_after}, serial_number: " \
-               f"{self.serial_number}, fingerprint: {self.fingerprint}, all_domains: {self.all_domains})"
+        return f"(subject: {self.subject}, issuer: {self.issuer}, extensions: {self.extensions}, not_before: {self.not_before}, not_after: {self.not_after}, serial_number: {self.serial_number}, fingerprint: {self.fingerprint}, all_domains: {self.all_domains})"
