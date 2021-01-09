1466cb14884d7092c98de9f2986470b5613c772f
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-09 04:01:11 +0100

    fix: change fields of cert update to match latest certstream update

--- certpwn/core/certstreamdata/update.py
+++ certpwn/core/certstreamdata/update.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from .certstreamobject import CertstreamObject
-from .chain import Chain
 from .leafcert import LeafCert
 
 
 class Update(CertstreamObject):
     """Data class for the certificate Update type from certstream"""
 
-    def __init__(self, update_type, leaf_cert, chain, cert_index, seen, source, raw_dict):
+    def __init__(self, update_type, leaf_cert, cert_index, cert_link, seen, source, raw_dict):
         """
         Data class for the certificate Update type from certstream
         :param update_type: The type of the certificate update
@@ -20,8 +19,8 @@ class Update(CertstreamObject):
         super().__init__()
         self.update_type = update_type
         self.leaf_cert = leaf_cert
-        self.chain = chain
         self.cert_index = cert_index
+        self.cert_link = cert_link
         self.seen = seen
         self.source = source
         self.all_domains = leaf_cert.all_domains if leaf_cert is not None else ""
@@ -41,15 +40,15 @@ class Update(CertstreamObject):
         data = super(Update, cls).from_dict(data)
         update_type = data.get("update_type")
         leaf_cert = LeafCert.from_dict(data.get("leaf_cert"))
-        chain = Chain.from_dict(data.get("chain"))
         cert_index = data.get("cert_index")
+        cert_link = data.get("cert_link")
         seen = data.get("seen")
         source = data.get("source")
 
         return cls(update_type=update_type,
                    leaf_cert=leaf_cert,
-                   chain=chain,
                    cert_index=cert_index,
+                   cert_link=cert_link,
                    seen=seen,
                    source=source,
                    raw_dict=raw_data)
