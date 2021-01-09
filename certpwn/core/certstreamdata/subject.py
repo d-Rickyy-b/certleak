edba2513e3f563b323337aa8b8a7ba6b71827415
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-09 04:37:11 +0100

    feat: add email address to subject class

--- certpwn/core/certstreamdata/subject.py
+++ certpwn/core/certstreamdata/subject.py
@@ -5,7 +5,7 @@ from .certstreamobject import CertstreamObject
 class Subject(CertstreamObject):
     """Data class for the Subject field of a certificate"""
 
-    def __init__(self, aggregated, c, st, l, o, ou, cn):
+    def __init__(self, aggregated, c, st, l, o, ou, cn, email_address):
         """
         Data class for the Subject field of a certificate
         :param aggregated: Aggregated string of the other RDNs
@@ -24,6 +24,7 @@ class Subject(CertstreamObject):
         self.O = o
         self.OU = ou
         self.CN = cn
+        self.email_address = email_address
 
     @classmethod
     def from_dict(cls, data):
@@ -43,8 +44,9 @@ class Subject(CertstreamObject):
         o = data.get("O")
         ou = data.get("OU")
         cn = data.get("CN")
+        email_address = data.get("emailAddress")
 
-        return cls(aggregated=aggregated, c=c, st=st, l=l, o=o, ou=ou, cn=cn)
+        return cls(aggregated=aggregated, c=c, st=st, l=l, o=o, ou=ou, cn=cn, email_address=email_address)
 
     def __repr__(self):
         return "Subject(C='%s', ST='%s', L='%s', O='%s', OU='%s', CN='%s' | aggregated='%s')" % (self.C, self.ST, self.L, self.O, self.OU, self.CN,
