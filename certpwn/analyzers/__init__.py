9e94ef68484b03d3c20f2611d775004f582c43aa
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-12 02:39:30 +0100

    refactor: reformat and add analyzer imports

--- certpwn/analyzers/__init__.py
+++ certpwn/analyzers/__init__.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from .alwaystrueanalyzer import AlwaysTrueAnalyzer
+from .authoritykeyidanalyzer import AuthorityKeyIDAnalyzer
 from .basicanalyzer import BasicAnalyzer
 from .cafingerprintanalyzer import CAFingerprintAnalyzer
+from .dnstwistanalyzer import DNStwistAnalyzer
+from .fulldomainanalyzer import FullDomainAnalyzer
 from .letsencryptanalyzer import LetsEncryptAnalyzer
 from .precertanalyzer import PreCertAnalyzer
 from .regexdomainanalyzer import RegexDomainAnalyzer
+from .tldanalyzer import TLDAnalyzer
+from .wildcardcertanalyzer import WildcardCertAnalyzer
 from .x509analyzer import X509Analyzer
 
-__all__ = ['BasicAnalyzer', 'AlwaysTrueAnalyzer', 'X509Analyzer', 'PreCertAnalyzer', 'CAFingerprintAnalyzer', 'LetsEncryptAnalyzer', 'RegexDomainAnalyzer']
+__all__ = ["BasicAnalyzer", "TLDAnalyzer", "FullDomainAnalyzer", "AlwaysTrueAnalyzer", "WildcardCertAnalyzer", "X509Analyzer", "PreCertAnalyzer",
+           "LetsEncryptAnalyzer", "RegexDomainAnalyzer", "AuthorityKeyIDAnalyzer", "DNStwistAnalyzer"]
