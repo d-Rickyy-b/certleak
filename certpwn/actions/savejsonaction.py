190d172fb3bbbea71a1ecf37beaaa33450073987
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-14 01:09:02 +0100

    feat: implement savejsonaction

---
+++ certpwn/actions/savejsonaction.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import json
+
+from .savefileaction import SaveFileAction
+
+
+class SaveJSONAction(SaveFileAction):
+    """Action to save a json formatted file to the disk"""
+    name = "SaveJSONAction"
+
+    def __init__(self, path):
+        super().__init__(path, file_ending=".json")
+
+    def get_file_content(self, update, analyzer_name, matches):
+        """Returns the content to be written to the file"""
+        return json.dumps(update.to_dict())
