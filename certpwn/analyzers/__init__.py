6ed1d50dbba987764ad507c98f1d113ee4e0b1cd
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-21 01:31:57 +0100

    feat: make analyzers available through module imports

--- certpwn/analyzers/__init__.py
+++ certpwn/analyzers/__init__.py
@@ -3,13 +3,15 @@ from .alwaystrueanalyzer import AlwaysTrueAnalyzer
 from .basicanalyzer import BasicAnalyzer
 from .cafingerprintanalyzer import CAFingerprintAnalyzer
 from .dnstwistanalyzer import DNStwistAnalyzer
+from .domainregexanalyzer import DomainRegexAnalyzer
 from .fulldomainanalyzer import FullDomainAnalyzer
 from .letsencryptanalyzer import LetsEncryptAnalyzer
 from .precertanalyzer import PreCertAnalyzer
 from .regexdomainanalyzer import RegexDomainAnalyzer
+from .subdomainanalyzer import SubDomainAnalyzer
 from .tldanalyzer import TLDAnalyzer
 from .wildcardcertanalyzer import WildcardCertAnalyzer
 from .x509analyzer import X509Analyzer
 
 __all__ = ["BasicAnalyzer", "TLDAnalyzer", "FullDomainAnalyzer", "AlwaysTrueAnalyzer", "WildcardCertAnalyzer", "X509Analyzer", "PreCertAnalyzer",
-           "LetsEncryptAnalyzer", "RegexDomainAnalyzer", "DNStwistAnalyzer"]
+           "LetsEncryptAnalyzer", "RegexDomainAnalyzer", "DNStwistAnalyzer", "DomainRegexAnalyzer", "SubDomainAnalyzer"]
