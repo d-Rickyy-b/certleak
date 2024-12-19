1a03852fdc6518f2065cc9f3ced2e2a36ee09100
Rico <d-Rickyy-b@users.noreply.github.com>
2024-12-19 21:49:40 +0100

    refactor: make __version__ available in package __init__.py

--- certleak/__init__.py
+++ certleak/__init__.py
@@ -2,7 +2,8 @@
 # Otherwise there will be circular imports -> bad
 
 from .core.certleak import CertLeak
+from .version import __version__
 
 __author__ = "d-Rickyy-b (certleak@rico-j.de)"
 
-__all__ = ["CertLeak", ]
+__all__ = ["CertLeak", "__version__"]
